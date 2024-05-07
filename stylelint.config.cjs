44ef1180add4cba4977e591ac2ab456fad1a2e68
xxxx <xxxxemail>
2024-05-07 15:45:18 +0800

    xxx

---
+++ stylelint.config.cjs
@@ -0,0 +1,9 @@
+/*eslint-env node */
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    // 'stylelint-config-recommended-scss',
+    'stylelint-config-prettier',
+    'stylelint-config-html/vue'
+  ]
+}
